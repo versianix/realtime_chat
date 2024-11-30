d1bba36239a4c84d9a80579496c20f9f330a429d
marcelversiani <marcelversiani25@gmail.com>
2024-11-30 17:22:31 -0300

    Update chat view to require login and fetch recent messages; modify chat template to display dynamic message content

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
+@login_required
 def chat_view(request):
-    return render(request, 'a_rtchat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages})
