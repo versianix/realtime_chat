a9659aa87c4c51d3e6a60f67851e8718793d2f11
marcelversiani <marcelversiani25@gmail.com>
2024-11-30 18:41:59 -0300

    Add channel layer configuration and enhance ChatRoomConsumer for group messaging

--- a_core/settings.py
+++ a_core/settings.py
@@ -90,6 +90,12 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'a_core.wsgi.application'
 ASGI_APPLICATION = 'a_core.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
