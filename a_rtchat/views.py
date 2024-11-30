037b2f81a7db1d7a8f045337acb826f67c64d803
marcelversiani <marcelversiani25@gmail.com>
2024-11-30 17:30:46 -0300

    Add form for creating chat messages and update chat view to handle submissions

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import *
 
 @login_required
 def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_messages = chat_group.chat_messages.all()[:30]
-    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages})
+    form = ChatMessageCreateForm()
+
+    if request.method == 'POST':
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect('home')
+        
+    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages, 'form': form})
