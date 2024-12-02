39586c5feededf0571bf9d2ee4df4cf1669e9cc5
marcelversiani <marcelversiani25@gmail.com>
2024-12-01 23:05:36 -0300

    Add online status functionality; implement OnlineStatusConsumer for real-time user tracking, update routing, and create online status template

--- a_rtchat/routing.py
+++ a_rtchat/routing.py
@@ -3,4 +3,5 @@ from .consumers import *
 
 websocket_urlpatterns = [
     path("ws/chatroom/<chatroom_name>", ChatRoomConsumer.as_asgi()),
+    path("ws/online-status/", OnlineStatusConsumer.as_asgi()),
 ]
